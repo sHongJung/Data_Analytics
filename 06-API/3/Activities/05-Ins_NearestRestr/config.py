bcc0cd10d04e2e244b9a5e6403158834af60aa5b
sHongJung <ken.sh.jung@gmail.com>
2018-03-09 14:49:52 -0800

    api

---
+++ 06-API/3/Activities/05-Ins_NearestRestr/config.py
@@ -0,0 +1,3 @@
+# This API key has been disabled, please use your own.
+
+gkey = "AIzaSyA_Clyz3478YAUnsESNHE5dyktvvMoa-vw"
---
+++ 06-API/3/Activities/07-Ins_Google_Radar/config.py
@@ -0,0 +1,3 @@
+# This API key has been disabled, please use your own.
+
+gkey = "AIzaSyAZPq0HmyC_qhmf4eKnfv9N4H9Y3ANlMXQ"
