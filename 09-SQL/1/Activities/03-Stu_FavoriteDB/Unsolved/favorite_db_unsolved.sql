8c2f18f68ec2fba2002942c71cf7b59fd2d99ba8
sHongJung <ken.sh.jung@gmail.com>
2018-04-09 17:28:22 -0700

    SQL

--- 09-SQL/1/Activities/03-Stu_FavoriteDB/Unsolved/favorite_db_unsolved.sql
+++ 09-SQL/1/Activities/03-Stu_FavoriteDB/Unsolved/favorite_db_unsolved.sql
@@ -1,27 +1,35 @@
 -- Drops the favorite_db if it exists currently --
-DROP DATABASE IF EXISTS favorite_db;
+-- DROP DATABASE IF EXISTS favorite_db;
 -- Creates the "favorite_db" database --
 CREATE DATABASE favorite_db;
 
 -- Make it so all of the following code will affect favorite_db --
+USE favorite_db;
 
 -- Creates the table "favorite_foods" within favorite_db --
 CREATE TABLE favorite_foods (
   -- Make a string column called "food" which cannot contain null --
+  food VARCHAR(50) NOT NULL,
   -- Make an numeric column called "score" --
+  score INTEGER(10)
 );
 
 CREATE TABLE favorite_songs (
   -- Make a string column called "song" which cannot contain null --
+  song VARCHAR(50) NOT NULL,
   -- Make a string column called "artist" --
+  artist VARCHAR(50)
   -- Make an integer column called "score" --
   score INTEGER(10)
 );
 
 CREATE TABLE favorite_movies (
   -- Create a numeric column called "id" which automatically increments and cannot be null --
+  
   -- Create a string column called "movie" which cannot be null --
+  moive VARCHAR(50) NOT NULL,
   -- Create a boolean column called "five_times" that sets the default value to false if nothing is entered --
+  five_time BOOLEAN
   -- Make an integer column called "score" --
   -- Set the primary key of the table to id --
 );
